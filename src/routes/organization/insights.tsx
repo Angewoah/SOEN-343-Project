4579ce934c6751577a220dbfd7a61904419d8c4c
William Charron-Boyle <williamcharronboyle@gmail.com>
2025-03-27 12:41:03 -0400

    fix insights display

--- src/routes/organization/insights.tsx
+++ src/routes/organization/insights.tsx
@@ -1,84 +1,67 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "@tanstack/react-router";
 import { createFileRoute } from "@tanstack/react-router";
+import { fetchEventBookings } from "../../modules/booking/service";
 import { fetchEvents } from "../../modules/event/service";
 import { getSupabaseClient } from "../../supabase/client";
 import { Sidebar } from "../../components/Sidebar";
 
 export const Route = createFileRoute("/organization/insights")({
   component: RouteComponent,
+  loader: async () => {
+    const { data, error } = await getSupabaseClient().auth.getUser();
+    
+    if (error || !data.user) {
+      return [];
+    }
+
+    const eventArr = await fetchEvents(data.user);
+    const bookingArr = await Promise.all(
+      eventArr.map(async (event) => {
+        return fetchEventBookings(event.id);
+      })
+    );
+
+    const result = eventArr.map((eventData, i) => {
+      const bookingData = bookingArr[i];
+      const confirmationData = bookingData.filter((booking) => 
+        booking.registration_status === "confirmed");
+      return {
+        event: eventData,
+        confirmations: confirmationData.length,
+        tentatives: bookingData.length - confirmationData.length,
+      };
+    });
+    
+    return result;
+  }
 });
 
 function RouteComponent() {
   const supabase = getSupabaseClient();
-  const events = useLoaderData({ from: "/organization/events" }) ?? [];
   const [eventId, setEventId] = useState(null);
-  const [attendeeList, setAttendeeList] = useState([]);
   const [query, setQuery] = useState("");
-
-  useEffect(() => {
-    if (eventId == null) {
-      return;
-    }
-    const fetchAttendeeIdArr = async (eventId: number) => {
-      const { data } = await supabase
-        .from("bookings")
-        .select("user_id")
-        .eq("event_id", eventId);
-      return data;
-    };
-    const fetchAttendee = async (attendeeId: number) => {
-      const { data } = await supabase
-        .from("profiles")
-        .select("*")
-        .eq("user_id", attendeeId);
-      return data;
-    };
-    const fetchAttendeeArr = async () => {
-      const attendeeIdArr = await fetchAttendeeIdArr(eventId);
-      const attendeesPromises = attendeeIdArr.map(async (attendee) => {
-        const attendeeData = await fetchAttendee(attendee.user_id);
-        return attendeeData[0];
-      });
-
-      const attendees = await Promise.all(attendeesPromises);
-      setAttendeeList(attendees);
-    };
-
-    fetchAttendeeArr();
-  }, [eventId]);
-
+  const eventData = useLoaderData({ from: "/organization/insights" }) ?? [];
+  
   return (
     <>
       <Sidebar />
       <div className="w-full flex flex-col px-72 py-4">
         <h1 className="text-4xl mb-10">Insights</h1>
-        <input
-          type="number"
-          placeholder="Enter event id"
-          value={query}
-          onChange={(e) => {
-            const value = e.target.value;
-            if (value !== "" && typeof +value === "number") {
-              setEventId(value);
-            }
-            setQuery(value);
-            return;
-          }}
-          className="w-full p-2 mb-4 border border-neutral-300 rounded-lg"
-        />
-        {attendeeList.length === 0 ? (
-          <p>No registrations exist for this event</p>
-        ) : (
-          attendeeList.map((attendee) => (
-            <div
-              key={attendee.id}
+        {eventData.map((entry) => {
+          const { event, confirmations, tentatives } = entry;
+          
+          return <div
+              key={event.id}
               className="border-2 border-neutral-300 p-4 mb-4 rounded-lg"
-            >
-              <h2 className="text-xl font-bold">{attendee.name}</h2>
+              >
+              <h2 className="text-center">{event.title}</h2>
+              <p>Description: {event.description}</p>
+              <p>Confirmed Registrations: {confirmations}</p>
+              <p>Tentative Registrations: {tentatives}</p>
+            {console.log(entry)}
             </div>
-          ))
-        )}
+        })}
       </div>
     </>
   );
