7a310b0d3a5866c11c8e4e4a95255cb508e8c1e0
William Charron-Boyle <williamcharronboyle@gmail.com>
2025-04-06 12:58:54 -0400

    add filter button in insights

--- src/routes/organization/insights.tsx
+++ src/routes/organization/insights.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, createFileRoute, Link } from "@tanstack/react-router";
+import { ChevronDownIcon } from "../../../node_modules/@heroicons/react/16/solid";
 import { fetchEventBookings } from "../../modules/booking/service";
 import { fetchEvents } from "../../modules/event/service";
 import { getSupabaseClient } from "../../supabase/client";
@@ -39,41 +40,88 @@ export const Route = createFileRoute("/organization/insights")({
   }
 });
 
+function Filter(props) {
+  return <div
+    className="hover:bg-gray-100 rounded-lg">By {props.name}</div>
+}
+
 function RouteComponent() {
   const eventData = useLoaderData({ from: "/organization/insights" }) ?? [];
+  const [filterDropdownVisible, setFilterDropdownVisible] = useState(false);
+  const [filterMode, setFilterMode] = useState("popularity");
+  const dropdown = ["popularity", "trending"].map((name, index) => {
+    return <li 
+      className="hover:bg-gray-100 rounded-lg" 
+      key={index}
+      onClick={() => {
+        setFilterMode(name);
+        setFilterDropdownVisible(false);
+      }}
+    >
+      By {name}
+    </li>
+  });
+  
   return (
     <>
       <Sidebar />
-      <div className="w-full flex flex-col px-72 py-4">
-        <h1 className="text-4xl mb-10">Insights</h1>
-        {eventData.map((entry) => {
-          const { event, confirmations, tentatives, attendees } = entry;
-          return (<div
-              key={event.id}
-              className="border-2 border-neutral-300 p-4 mb-4 rounded-lg"
+      <div className="w-full px-72 justify-end-safe pt-4">
+        <h1 className="text-4xl">Insights</h1>
+        <div className="mt-10">
+          <button 
+            type="button" 
+            className="font-medium text-md text-white bg-purple-500 hover:bg-purple-700 p-2 border-2 rounded-lg transition-colors text-center flex flex-row place-content-between items-center cursor-pointer w-32"
+            onClick={() => {
+              setFilterDropdownVisible(!filterDropdownVisible);
+            }}
+          >
+            Sort 
+            <ChevronDownIcon className="w-4"/>
+          </button>
+          <div 
+            className="max-w-32 cursor-pointer"
+            onClick={() => {
+              
+            }}
+          >
+            { filterDropdownVisible ? 
+              <ul className="border-2 rounded-lg border-neutral-300">
+                {dropdown}
+              </ul>
+              : ""
+            }
+          </div>
+          {eventData.map((entry) => {
+            const { event, confirmations, tentatives, attendees } = entry;
+            return (
+              <div
+                key={event.id}
+                className="border-2 border-neutral-300 rounded-lg my-4"
               >
-              <h2 className="text-center">{event.title}</h2>
-              <div>
-                <p>Description: {event.description}</p>
-                <p>Status: {event.status}</p>
-                <p>Confirmed Registrations: {confirmations}</p>
-                <p>Pending Registrations: {tentatives}</p>
-                <p>
-                  Total Amount of Attendees: {attendees} 
-                  /{event.max_attendees}
-                </p>
+                <h2 className="text-center">{event.title}</h2>
+                <div>
+                  <p>Description: {event.description}</p>
+                  <p>Status: {event.status}</p>
+                  <p>Confirmed Registrations: {confirmations}</p>
+                  <p>Pending Registrations: {tentatives}</p>
+                  <p>
+                    Total Amount of Attendees: {attendees} 
+                    /{event.max_attendees}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-2 w-full max-w-48">
+                  <Link
+                    to="/organization/report/$eventId"
+                    params={{ eventId: `${event.id}` }}
+                    className="font-medium text-md text-white bg-purple-500 hover:bg-purple-700 p-2 border-2 rounded-lg transition-colors text-center"
+                  >
+                    See Report
+                  </Link>
+                </div>
               </div>
-              <div className="flex flex-col gap-2 w-full max-w-48">
-                <Link
-                  to="/organization/report/$eventId"
-                  params={{ eventId: `${event.id}` }}
-                  className="font-medium text-md text-white bg-purple-500 hover:bg-purple-700 p-2 border-2 rounded-lg transition-colors text-center"
-                >
-                  See Report
-                </Link>
-              </div>
-            </div>);
-        })}
+            );
+          })}
+        </div>
       </div>
     </>
   );
