2d2845f05eb22f4154244192b1f6bc6613717f34
Merouane Issad <merouaneissad@gmail.com>
2025-04-08 04:46:33 -0400

    + Added new interests parametersto users + Improved emailing system to send personalised messages + Added new email templates, titles and messages for different types of event

--- src/modules/mail/mailService.ts
+++ src/modules/mail/mailService.ts
@@ -1,29 +1,106 @@
 import emailjs from '@emailjs/browser';
+import { getUserProfilesWithInterests } from "../../modules/profile/service"
 
-export const sendEventPromotionMail = async (eventTitle: String, eventDescription: String, eventLink: String, emailRecipient: String) => {
-    
+export const sendEventPromotionMail = async (eventTitle: string, eventDescription: string, eventTag: string, eventLink: string, emailRecipient: string) => {
     emailjs.init({
-        publicKey: 'RXKJqHFZaTv8DcPeE',
-        // Do not allow headless browsers
-        blockHeadless: true
+      publicKey: 'RXKJqHFZaTv8DcPeE',
+      // Do not allow headless browsers
+      blockHeadless: true
     });
 
-    var templateParams = {
-        EventTitle: eventTitle,
-        EventDescription: eventDescription,
-        EventLink: eventLink,
-        'email': emailRecipient
-    };
+    if(emailRecipient)
+    {
+      let tagArray = eventTag.split(',').map(str => str.trim());
+      let selectedTags = tagArray.filter((tag) => "educational,entertainment,networking".includes(tag));
+      let selectedTag = selectedTags[Math.floor(Math.random() * selectedTags.length)];
+      if(selectedTag)
+      {
+        var templateParams = {
+          EmailTitle: getEmailTitle(selectedTag),
+          EventTitle: eventTitle,
+          EventDescription: eventDescription,
+          EventLink: eventLink,
+          'email': emailRecipient
+        };
 
-    emailjs.send('service_856qqh8', 'template_5greirm', templateParams)
-    .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-    
-    console.log("sent mail!");
+        emailjs.send('service_856qqh8', getEmailTemplate(selectedTag), templateParams)
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+      }
+    }
+    else
+    {
+      getUserProfilesWithInterests(eventTag)
+      .then( (profiles) => {
+        if(profiles && profiles.length > 0)
+        {
+          profiles.forEach(function (profile, index) {
+
+            let tagArray = eventTag.split(',').map(str => str.trim());
+            let selectedTags = tagArray.filter((tag) => profile.interests?.includes(tag));
+            let selectedTag = selectedTags[Math.floor(Math.random() * selectedTags.length)];
+            if(selectedTag)
+            {
+              var templateParams = {
+                EmailTitle: getEmailTitle(selectedTag),
+                EventTitle: eventTitle,
+                EventDescription: eventDescription,
+                EventLink: eventLink,
+                'email': profile.email
+              };
+  
+              emailjs.send('service_856qqh8', getEmailTemplate(selectedTag), templateParams)
+              .then(
+                () => {
+                  console.log('SUCCESS!');
+                },
+                (error) => {
+                  console.log('FAILED...', error.text);
+                },
+              );
+            }
+          });
+        }
+        else
+        {
+          console.log("No user found with the correct interests");
+        }
+      });
+    }
+}
+
+function getEmailTemplate(tag: string)
+{
+  switch(tag)
+  {
+    case 'educational':
+      return 'template_5greirm';
+    case 'entertainment':
+      return 'template_62db823';
+    case 'networking':
+      return 'template_62db823';
+    default:
+      return 'template_5greirm';
+  }
+}
+
+function getEmailTitle(tag: string)
+{
+  switch(tag)
+  {
+    case 'educational':
+      return  'A new learning opportunity awaits you!';
+    case 'entertainment':
+      return  "Don't miss this fun activity happening soon!";
+    case 'networking':
+      return  'Come meet new people in this new event!';
+    default:
+      return 'Do not miss this new event!';
+  }
 }
